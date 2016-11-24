4c9152a9a3c101db73041ed36a4ef4825d1b3f08
PharrellWANG <15113029g@connect.polyu.hk>
2016-11-24 18:37:51 +0800

    weather api combined two days and keep 8 hours

--- 4-weather.py
+++ 4-weather.py
@@ -3,23 +3,30 @@ from urllib.request import Request, urlopen
 import re
 import json
 
-req = Request('http://m.accuweather.com/en/hk/hong-kong/1123655/hourly-weather-forecast/1123655?day=2')
-page = urlopen(req).read()
+req1 = Request('http://m.accuweather.com/en/hk/hong-kong/1123655/hourly-weather-forecast/1123655?day=1')
+page1 = urlopen(req1).read()
 
-soup = BeautifulSoup(page, 'lxml')
+req2 = Request('http://m.accuweather.com/en/hk/hong-kong/1123655/hourly-weather-forecast/1123655?day=2')
+page2 = urlopen(req2).read()
 
-letters = soup.find_all("div", class_="wx-cell")
+soup1 = BeautifulSoup(page1, 'lxml')
+soup2 = BeautifulSoup(page2, 'lxml')
 
+letters1 = soup1.find_all("div", class_="wx-cell")
+
+letters2 = soup2.find_all("div", class_="wx-cell")
+
+# ======================================================
 result = []
 status_code = 0  # only status_code = 0 is good to go
 
 hour_format = re.compile('.{2}AM' or '.AM')
 
-for element in letters:
+for element1 in letters1:
     t = {}
     s = {}
-    tag = element
-    hour = (element.a.get_text())[0:4]
+    tag = element1
+    hour = (element1.a.get_text())[0:4]
     temp = (tag.find(class_="temp").get_text())[0:2]
     LastTwoCharInHour = hour[-2:]
     if LastTwoCharInHour == 'AM' or 'M':
@@ -36,6 +43,30 @@ for element in letters:
         status_code = 2  # temp is not an int number
         break
     result.append(t)
+
+for element2 in letters2:
+    t = {}
+    s = {}
+    tag = element2
+    hour = (element2.a.get_text())[0:4]
+    temp = (tag.find(class_="temp").get_text())[0:2]
+    LastTwoCharInHour = hour[-2:]
+    if LastTwoCharInHour == 'AM' or 'M':
+        t["time"] = hour
+        status_code = 0
+    else:
+        status_code = 1
+        break
+    try:
+        temp_val = int(temp)
+        t["temperature"] = temp
+        status_code = 0
+    except ValueError:
+        status_code = 2  # temp is not an int number
+        break
+    result.append(t)
+# print(type(result))
+result = result[0:8]
 if status_code == 0:
     print(json.dumps({"DataList": {'Root': result}}, sort_keys=False))
 else:
